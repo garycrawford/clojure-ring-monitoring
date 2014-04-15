8d6d330ad61101a67ab44a8d4e708eb2be4ed548
garycrawford <bigdaddy@garycrawford.co.uk>
2014-04-15 14:35:28 +0100

    removed un-needed deps.

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
 (defproject clojure-ring-monitoring "0.1.0-SNAPSHOT"
   :description "Add healthchecks to your app"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "4.0.1"]
-                 [metrics-clojure "1.0.1" :exclusions [cheshire org.clojure/clojure]]
-                 [metrics-clojure-ring "1.0.1" :exclusions [cheshire org.clojure/clojure]]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [ch.qos.logback/logback-classic "1.1.1"]
                  [ring/ring-core "1.1.5" :exclusions [org.clojure/clojure]]
                  [hiccup "1.0.0" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-swank "1.4.4"]]
